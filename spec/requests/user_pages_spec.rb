2b7f9a22b2c759497b824f4899b89304e8bfae2d
belozi <robert.belozi.lee@gmail.com>
2014-07-09 22:32:39 -0400

    Add user microposts

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -56,11 +56,20 @@ describe "User pages" do
 
 	describe "profile page" do 
 		let(:user) 	{ FactoryGirl.create(:user) }
+		let!(:m1)	{ FactoryGirl.create(:micropost, user: user, content: "Foo") }
+		let!(:m2)	{ FactoryGirl.create(:micropost, user: user, content: "Bar") }
+
 		before { visit user_path(user) }
 
 		it { should have_content(user.first_name) }
 		it { should have_content(user.last_name) }
 		it { should have_title("#{user.first_name} #{user.last_name}") }
+
+		describe "microposts" do 
+			it { should have_content(m1.content) }
+			it { should have_content(m2.content) }
+			it { should have_content(user.microposts.count) }
+		end
 	end
 
 	describe "signup page" do
